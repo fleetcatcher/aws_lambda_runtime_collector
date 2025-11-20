348c7658e3f996c39f301eec913c1fd366515b1e
Miklos Halasz <miklos.halasz@platformscience.com>
2025-11-20 10:36:12 +0100

    list only lambda functions that have python runtime

--- aws_lambda_runtime_collector.sh
+++ aws_lambda_runtime_collector.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+set -e
+
 export AWS_PAGER=""
 
 regions=$(aws ec2 describe-regions --query 'Regions[].RegionName' --output text)
 for region in ${regions}; do
     aws lambda list-functions --region "${region}" \
-        --query "Functions[].join(',', ['${region}',FunctionName,FunctionArn,Runtime,LastModified])" \
-        --output text
+        --query "Functions[?starts_with(Runtime, 'python')]" \
+        --output json | \
+        jq -r ".[] | [.FunctionName, .FunctionArn, .Runtime, .LastModified] | @csv"
 done
 
